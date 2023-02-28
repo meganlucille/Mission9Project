ab9a3e8a000892507fb6e75f98112513b6136e4f
Megan Johnson <megan@mightyjohnsons.com>
2023-02-28 12:03:40 -0700

    Add project files.

---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9Project.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumProjects { get; set; }
+        public int ProjectsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumProjects / ProjectsPerPage);
+
+    }
+}
